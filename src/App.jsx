2eaf7c04ebf9fb73b75395a4a4d8876010e91d6c
Hamin <hm5172@naver.com>
2024-02-05 16:40:18 +0900

    feat: 업로드 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
+import Update from "./Pages/Update";
+import Edit from "./Pages/Edit";
+
 import './App.css';
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
           <Routes>
             <Route path="/Login" element = {<Login/>}/>
             <Route path="/SignUp" element = {<SignUp/>}/>
+            <Route path="/Update" element = {<Update/>}/>
+            <Route path="/Edit" element = {<Edit/>}/>
           
           </Routes>
     </BrowserRouter>
