820b39299d30c31fd3cff9ffda5825d593e5fb95
이경은 <saroojamin13@gmail.com>
2024-03-19 18:05:42 +0900

    feat:#3 음악 앨범 뒷부분 플레이어 디자인

--- src/Components/MusicPlayer.jsx
+++ src/Components/MusicPlayer.jsx
@@ -1,4 +1,67 @@
 import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+
+const AudioPlayerContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProgressBarContainer = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const ProgressBar = styled.input`
+  width: 100%;
+  height: 4px;
+  -webkit-appearance: none;
+  background: #white;
+  outline: none;
+  border-radius: 10px;
+  margin-bottom: 5px;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 10px;
+    height: 10px;
+    background: #fff;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  &::-moz-range-thumb {
+    width: 10px;
+    height: 10px;
+    background: #fff;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  &::-ms-thumb {
+    width: 10px;
+    height: 10px;
+    background: #fff;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+`;
+
+const TimeLabels = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.8em;
+  color: #999;
+`;
+
+const PlayButton = styled.div`
+  font-size: 24px;
+  cursor: pointer;
+  margin-top: 50px;
+`;
 
 const AudioPlayer = ({ audioSrc }) => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -37,28 +100,30 @@ const AudioPlayer = ({ audioSrc }) => {
   };
 
   return (
-    <div>
+    <AudioPlayerContainer>
+      <PlayButton onClick={togglePlay}>
+        {isPlaying ? '⏸️' : '▶'}
+      </PlayButton>
+      <ProgressBarContainer>
+        <ProgressBar
+          type="range"
+          value={currentTime}
+          max={duration || 0}
+          onChange={handleSeek}
+        />
+        <TimeLabels>
+          <span>{formatTime(currentTime)}</span>
+          <span>{formatTime(duration)}</span>
+        </TimeLabels>
+      </ProgressBarContainer>
+      
       <audio
         ref={audioPlayer}
         src={audioSrc}
         onTimeUpdate={handleTimeUpdate}
         onLoadedMetadata={updateTime}
       />
-      <div>
-        <button onClick={togglePlay}>
-          {isPlaying ? '⏸️' : '▶'}
-        </button>
-        <input
-          type="range"
-          value={currentTime}
-          max={duration || 0}
-          onChange={handleSeek}
-        />
-        <div>
-          {formatTime(currentTime)} / {formatTime(duration)}
-        </div>
-      </div>
-    </div>
+    </AudioPlayerContainer>
   );
 };
 
